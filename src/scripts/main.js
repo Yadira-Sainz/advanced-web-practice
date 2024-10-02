4a3cafb01e93ee0bcf9c38f897444319414f5a0b
YadiraSainz <sainz_06@outlook.com>
2024-10-02 12:44:57 -0600

    Adding decrement button of counter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,21 +3,29 @@ const app = document.getElementById('app');
 const createCounter = () => {
     let count = 0;
     const counterElement = document.createElement('div');
-    const buttonElement = document.createElement('button');
+    const incrementButton = document.createElement('button');
+    const decrementButton = document.createElement('button');
 
     const updateCounter = () => {
         counterElement.textContent = `Contador: ${count}`;
     };
 
-    buttonElement.textContent = 'Incrementar';
-    buttonElement.addEventListener('click', () => {
+    incrementButton.textContent = 'Incrementar';
+    incrementButton.addEventListener('click', () => {
         count++;
+        updateCounter();
+    });
+
+    decrementButton.textContent = 'Decrementar';
+    decrementButton.addEventListener('click', () => {
+        count--;
         updateCounter();
     });
 
     updateCounter();
     app.appendChild(counterElement);
-    app.appendChild(buttonElement);
+    app.appendChild(incrementButton);
+    app.appendChild(decrementButton);
 };
 
 createCounter();
